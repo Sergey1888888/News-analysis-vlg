bc77144a95353701e0dec452ddc97226ee76995f
Viktor Serdyukov <gigabyte.091@gmail.com>
2020-06-06 04:48:40 +0300

    Закончил с томитой

--- tomita/tomita.py
+++ tomita/tomita.py
@@ -5,36 +5,41 @@ import sys
 sys.path.append("..")
 from modeldb import Connection, Mongo
 
-conn = Connection('45.11.24.111', 27017, 'passw0rd', 'mongo-root').getConnection()
-db = Mongo(conn,'news')
+def tomita():
+    conn = Connection('45.11.24.111', 27017, 'passw0rd', 'mongo-root').getConnection()
+    db = Mongo(conn,'news')
 
-textForAnalysis = db.find_document('data', {'forAnalysis': False}, True)
+    textForAnalysis = db.find_document('data', {'forAnalysis': True}, True)
+    if len(textForAnalysis) > 0:
+        # запись текста новостей в файл для томиты
+        f = open('input.txt', 'w')
+        for text in textForAnalysis:
+            
+            f.write(text.get("newsText")+'\n')
+        f.close()
 
-# запись текста новостей в файл для томиты
-f = open('input.txt', 'w')
-for text in textForAnalysis:
-   
-    f.write(text.get("newsText")+'\n')
+        # запуск томиты и получение данных
+        p = subprocess.Popen(["tomita-parser", "config.proto"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        out = out.decode("utf-8", "strict")
+        result = re.findall(r'(Person|Attractions)[\n\s{]*(FIO[\s=а-яёА-яa-zA-Z0-9]+|Name[\s=а-яА-яёa-zA-Z0-9]+)(Text[\s=а-яА-яёa-z0-9№A-Z,!.?\"\-—–]+)',out )
+        
+        facts = []
+        for fact in result:
+            if fact[0] == 'Person':
+                a = fact[1][6:-3]
+            elif  fact[0] == 'Attractions':
+                a = fact[1][7:-3]
+            b = a.split(' ')
+            str = '_'.join(b)
+            c = fact[2][7:-2]
+            mystr = c.replace(a,str)
+            facts.append({'newsWithMention': mystr})
 
-f.close()
+        conn['news'].data.update_many({'forAnalysis': True}, {"$set":{ 'forAnalysis': False}})
+        db.insert_document('analysis',facts,True)
+    else: 
+        print('Нету новый новостей')
 
-# запуск томиты и получение данных
-p = subprocess.Popen(["tomita-parser", "config.proto"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err = p.communicate()
-out = out.decode("utf-8", "strict")
-
-
-result = re.findall(r'(Person|Attractions)[\n\s{]*(FIO[\s=а-яёА-яa-zA-Z0-9]+|Name[\s=а-яА-яёa-zA-Z0-9]+)(Text[\s=а-яА-яёa-z0-9№A-Z,!.?\"\-—–]+)',out )
-facts = []
-for fact in result:
-    if fact[0] == 'Person':
-        a = fact[1][6:-3]
-    elif  fact[0] == 'Attractions':
-        a = fact[1][7:-3]
-    b = a.split(' ')
-    str = '_'.join(b)
-    c = fact[2][7:-2]
-    mystr = c.replace(a,str)
-    facts.append({'newsWithMention': mystr})
-
-db.insert_document('test',facts,True)
+if __name__ == '__main__':
+    tomita()
