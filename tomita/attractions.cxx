ffba28523195e0715a4784fa4077342814dfa966
Viktor Serdyukov <gigabyte.091@gmail.com>
2020-06-10 08:03:26 +0300

    немного передал томиту и удалил бин файлы

--- tomita/attractions.cxx
+++
@@ -1,5 +0,0 @@
-#encoding "utf-8"
-
-S -> Word<kwtype="дост_названия"> interp (Attractions.Name::not_norm);
-Text -> AnyWord* S AnyWord*;
-Text2 -> Text interp (Attractions.Text::not_norm);
--- tomita/tomita.py
+++ tomita/tomita.py
@@ -2,44 +2,45 @@ from pymongo import MongoClient
 import subprocess
 import re
 import sys
-sys.path.append("..")
-from modeldb import Connection, Mongo
+from per_attract_to_txt import checkPersons, checkPlaces
 
-def tomita():
-    conn = Connection('45.11.24.111', 27017, 'passw0rd', 'mongo-root').getConnection()
-    db = Mongo(conn,'news')
+def tomita(db):
 
-    textForAnalysis = db.find_document('data', {'forAnalysis': True}, True)
-    if len(textForAnalysis) > 0:
-        # запись текста новостей в файл для томиты
-        f = open('input.txt', 'w')
-        for text in textForAnalysis:
+    textForAnalysis = db.data.find({"forAnalysis": True})
+    if textForAnalysis.count(True) > 0:
+
+        i = 0
+        for fact in textForAnalysis:
+            print(i)
+            i = 1+i
+            f = open('input.txt', 'w')
+            f.write(fact.get("newsText"))
+            f.close()
+            p = subprocess.Popen(["tomita-parser", "config.proto"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = p.communicate()
+            out = out.decode("utf-8", "strict")
+            result = re.findall(r'(Person|Attractions)[\n\s{]*(FIO[\s=а-яёА-яa-zA-Z0-9]+|Name[\s=а-яА-яёa-zA-Z0-9]+)(Text[\s=а-яА-яёa-z0-9№A-Z,!.?\"\-—–]+)',out )
+            b.data.update_one({"_id": fact.get("_id")},{"$set":{"forAnalysis": False}})
+
+            if not result:
+                continue
             
-            f.write(text.get("newsText")+'\n')
-        f.close()
+            if result[0][0] == 'Person':
+                a = result[0][0]
+            elif  result[0][0] == 'Attractions':
+                a = result[0][0]
 
-        # запуск томиты и получение данных
-        p = subprocess.Popen(["tomita-parser", "config.proto"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        out = out.decode("utf-8", "strict")
-        result = re.findall(r'(Person|Attractions)[\n\s{]*(FIO[\s=а-яёА-яa-zA-Z0-9]+|Name[\s=а-яА-яёa-zA-Z0-9]+)(Text[\s=а-яА-яёa-z0-9№A-Z,!.?\"\-—–]+)',out )
-        
-        facts = []
-        for fact in result:
-            if fact[0] == 'Person':
-                a = fact[1][6:-3]
-            elif  fact[0] == 'Attractions':
-                a = fact[1][7:-3]
-
-
-            c = fact[2][7:-2]
-
-            facts.append({'newsWithMention': c})
-
-        conn['news'].data.update_many({'forAnalysis': True}, {"$set":{ 'forAnalysis': False}})
-        db.insert_document('analysis',facts,True)
+            c = result[0][2][7:-2]
+            db.analysis.insert_one({'_id': fact.get('_id'),'newsWithMention': c})
+            
     else: 
         print('Нету новый новостей')
 
+def check(parameter_list):
+    pass
 if __name__ == '__main__':
-    tomita()
+    client = MongoClient('45.11.24.111', username='mongo-root', password='passw0rd', authSource='admin')
+    db = client.news
+    checkPersons(db)
+    checkPlaces(db)
+    tomita(db)
