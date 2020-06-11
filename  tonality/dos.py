a7a1420ba998eb4f10a8faba766d25c993f63895
Sergey1888888 <Sergey1888888@gmail.com>
2020-06-11 15:24:49 +0400

    db connect and regexp for results

---  tonality/dos.py
+++  tonality/dos.py
@@ -1,8 +1,12 @@
 from dostoevsky.tokenization import RegexTokenizer
 from dostoevsky.models import FastTextSocialNetworkModel
+from pymongo import MongoClient
+import re
 
-#pip install dostoevsky
-#Просто как вы простили через достаевского
+# client = MongoClient('45.11.24.111', username='mongo-root', password='passw0rd', authSource='admin')
+# db = client.news
+# data = db.analysis
+# result = data.find({})
 
 tokenizer = RegexTokenizer()
 #tokens = tokenizer.split('всё очень плохо')  # [('всё', None), ('очень', None), ('плохо', None)]
@@ -12,12 +16,14 @@ model = FastTextSocialNetworkModel(tokenizer=tokenizer)
 #сюда текст
 messages = [
     'На сегодняшнем брифинге замглавы облздрава Николай Алимов уточнил, что в инфекционных госпиталях сейчас находятся 1680 пациентов с подтвержденным коронавирусом.',
-    'Об этом в среду рассказал зампредседателя комитета здравоохранения Волгоградской области Николай Алимов.'
+    'Об этом в среду рассказал зампредседателя комитета здравоохранения Волгоградской области Николай Алимов.',
+    'Я тебя люблю!'
 ]
 
 results = model.predict(messages, k=1)
 
-for message, sentiment in zip(messages, results):
+regex = r"[\w]+\(\['([\w]+)'\]\)"
 # результат
-
-    print(sentiment.keys())
+for message, sentiment in zip(messages, results):
+    match = re.search(regex, str(sentiment.keys()))
+    print(match.group(1))
