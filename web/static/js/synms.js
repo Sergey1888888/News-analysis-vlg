32e6ce370cb974d34fc4e47eade162310bf0bbfc
Sergey1888888 <sergey1888888@gmail.com>
2020-06-15 22:06:16 +0400

    update

--- web/static/js/synms.js
+++ web/static/js/synms.js
@@ -1,29 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
     const word = document.getElementById('word')
     const synm = document.getElementById('syn')
-
     synm.addEventListener('click',async(event)=>{
         const res = await fetch(`/api/getSynm/${word.value}`)
         console.log(res)
         const words = await res.json()
-        
         addNewWords(words)
     })
-
-
 })
 
 
-function addNewWords(words){
+function addNewWords(words) {
     const cont = document.getElementsByTagName('tbody')[0]
-
- 
-    for(let a of words){
-        let tr = document.createElement('tr')
-        tr.innerHTML = `<td>${a}</td> `
-        cont.appendChild(tr)
-          
+    const elements = document.getElementsByTagName('tr')
+    if (elements.length > 0) {
+        for(let el of elements) {
+            el.remove()
+        }
+        for(let a of words) {
+            let tr = document.createElement('tr')
+            tr.innerHTML = `<td>${a}</td> `
+            cont.appendChild(tr)
+        }
     }
-
-
+    else {
+        for(let a of words) {
+            let tr = document.createElement('tr')
+            tr.innerHTML = `<td>${a}</td> `
+            cont.appendChild(tr)
+        }
+    }
 }
